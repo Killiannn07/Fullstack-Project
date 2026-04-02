44ff9c2ccd67eec925c99078d306dd64787eec20
Killiannn07 <sofyanyunianto.work@gmail.com>
2026-04-02 16:21:59 +0700

    ww

--- Frontend/src/pages/ResetPassword.jsx
+++ Frontend/src/pages/ResetPassword.jsx
@@ -1,10 +1,8 @@
 import ResetPassword from "../components/layout/ResetPassword";
-import Container from "../components/layout/Container";
+
 
 export default function ResetPasswordPage() {
   return (
-    <Container>
-      <ResetPassword />
-    </Container>
+    <ResetPassword />
   );
 }
